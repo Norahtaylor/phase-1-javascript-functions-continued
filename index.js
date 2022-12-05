0f436f50f8f652ab919e568ac1851375d1eb0e94
[Norah Taylor] <emt368@gmail.com>
2022-12-05 15:03:17 -0500

    commit tree 1f21310ac4fbdf1dbf7c0ba052765635af8102e6

--- index.js
+++ index.js
@@ -1,1 +1,106 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(doWork = "go to the office"){
+    return "This Monday, I will" + doWork +"."
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saturdayFun(activity = "roller-skate"){
+//          return `This Saturday, I want to ${activity}!` 
+//     }
+
+// function mondayWork(activity = "go to the office"){
+//     return `This Monday, I will ${activity}.`
+// }
+
+// function wrapAdjective(style = "*", styleTwo = "||"){
+//     return function(first= "special") {
+//         return `You are ${style}${first}${style}!`
+//     }
+// }
+
+
+// //return `You are ${styleTwo} ${first} ${styleTwo}!`
+
+
+
+
+
+
+
+
+
+// function wrapAdjective(name) {
+//     const partOne = "special";
+//     return function () {
+//         const partTwo = "You are";
+//         return function() {
+//             const partThree = "a hard worker"; 
+//             return function() {
+//                 const partFour = "a dedicatied programmer"
+//                 return funtion () {
+//                     console.log()
+//                 }
+//             }
+//         }
+//         {
+//             console.log(`${partTwo} ${partOne.toUpperCase()}` )
+//         }
+//     }
+// }
+
+// function wrapAdjective(*)
+// const wrapAdjective = "You are ";
+
